310b616c63f0c1f08ff6b61d57506087dc2b6660
Yasir Malik <yasirrafiq786@gmail.com>
2020-08-11 16:13:20 +1000

    making header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,19 +5,19 @@ import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 const App = () => {
   return (
     <div>
-        <h1>Header!</h1>
+      <Header />
       <BrowserRouter>
         <div>
-            <Route path="/" exact component={StreamList} />
-            <Route path="/streams/new" exact component={StreamCreate} />
-            <Route path="/streams/edit" exact component={StreamEdit} />
-            <Route path="/streams/delete" exact component={StreamDelete} />
-            <Route path="/streams/show" exact component={StreamShow} />
-
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="ui secondary pointing menu">
+      <Link to="/" className="item">
+        Streamer
+      </Link>
+      <div className="right menu"></div>
+    </div>
+  );
 };
 
 export default Header;
