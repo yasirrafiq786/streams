2ae42de70efcd39dfdf20bcb9c9d45bb4d6f3230
Yasir Malik <yasirrafiq786@gmail.com>
2020-08-12 14:58:06 +1000

    added event listener to signed in status

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -14,22 +14,32 @@ class GoogleAuth extends React.Component {
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
           this.setState({isSignedIn: this.auth.isSignedIn.get()});
+          this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
   }
 
+  onAuthChange = () => {
+    this.setState({isSignedIn: this.auth.isSignedIn.get()});
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
-      return <div>I dont know</div>;
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div>I am signed in</div>;
+      return (
+          <button className="ui red google button">
+              <i className="google icon" />
+              Sign Out
+          </button>
+      );
     } else {
       return <div>I am not signed in</div>;
     }
   }
 
   render() {
-  return <div>{this.renderAuthButton()}</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
