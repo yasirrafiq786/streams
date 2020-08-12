561fc4dfaba2bf3d82e450033bbc387287ac1569
Yasir Malik <yasirrafiq786@gmail.com>
2020-08-12 15:28:08 +1000

    added redux and set it up

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {signIn, signOut} from '../actions';
 
 class GoogleAuth extends React.Component {
   state = {isSignedIn: null};
@@ -23,18 +25,31 @@ class GoogleAuth extends React.Component {
     this.setState({isSignedIn: this.auth.isSignedIn.get()});
   };
 
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
+
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-          <button className="ui red google button">
-              <i className="google icon" />
-              Sign Out
-          </button>
+        <button onClick={this.onSignOutClick} className="ui red google button">
+          <i className="google icon" />
+          Sign Out
+        </button>
       );
     } else {
-      return <div>I am not signed in</div>;
+      return (
+        <button onClick={this.onSignInClick} className="ui red google button">
+          <i className="google icon" />
+          Sign In
+        </button>
+      );
     }
   }
 
@@ -43,4 +58,7 @@ class GoogleAuth extends React.Component {
   }
 }
 
-export default GoogleAuth;
+export default connect(null, {
+  signIn,
+  signOut,
+})(GoogleAuth);
---
+++ client/src/reducers/index.js
@@ -0,0 +1,5 @@
+import {combineReducers} from 'redux';
+
+export default combineReducers({
+  replaceme: () => 1234,
+});
