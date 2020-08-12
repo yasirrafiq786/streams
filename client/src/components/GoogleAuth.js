fbf922f260a7862a47fefb885ef3c5301f4c7ffd
Yasir Malik <yasirrafiq786@gmail.com>
2020-08-12 15:51:46 +1000

    redux refactor in google auth working

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -3,8 +3,6 @@ import {connect} from 'react-redux';
 import {signIn, signOut} from '../actions';
 
 class GoogleAuth extends React.Component {
-  state = {isSignedIn: null};
-
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client
@@ -15,7 +13,7 @@ class GoogleAuth extends React.Component {
         })
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
-          this.setState({isSignedIn: this.auth.isSignedIn.get()});
+          this.onAuthChange(this.auth.isSignedIn.get());
           this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
@@ -34,9 +32,9 @@ class GoogleAuth extends React.Component {
   };
 
   renderAuthButton() {
-    if (this.state.isSignedIn === null) {
+    if (this.props.isSignedIn === null) {
       return null;
-    } else if (this.state.isSignedIn) {
+    } else if (this.props.isSignedIn) {
       return (
         <button onClick={this.onSignOutClick} className="ui red google button">
           <i className="google icon" />
@@ -58,7 +56,11 @@ class GoogleAuth extends React.Component {
   }
 }
 
-export default connect(null, {
+const mapStateToProps = (state) => {
+  return {isSignedIn: state.auth.isSignedIn};
+};
+
+export default connect(mapStateToProps, {
   signIn,
   signOut,
 })(GoogleAuth);
