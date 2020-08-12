0e5c46c53aa5214adbfef0bfc8731644a6d51b8f
Yasir Malik <yasirrafiq786@gmail.com>
2020-08-12 19:51:01 +1000

    completed google oauth setup

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -20,7 +20,9 @@ class GoogleAuth extends React.Component {
   }
 
   onAuthChange = (isSignedIn) => {
-    isSignedIn ? this.props.signIn() : this.props.signOut();
+    isSignedIn
+      ? this.props.signIn(this.auth.currentUser.get().getId())
+      : this.props.signOut();
   };
 
   onSignInClick = () => {
