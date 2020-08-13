0384bfd2e95c72e7f2af6d7ef9d7a8e11367df42
Yasir Malik <yasirrafiq786@gmail.com>
2020-08-13 19:17:09 +1000

    added thunk

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,9 +1,10 @@
-import { SIGN_IN, SIGN_OUT } from './types';
+import streams from '../apis/streams';
+import {SIGN_IN, SIGN_OUT} from './types';
 
 export const signIn = (userId) => {
   return {
     type: SIGN_IN,
-    payload: userId
+    payload: userId,
   };
 };
 
@@ -11,4 +12,8 @@ export const signOut = () => {
   return {
     type: SIGN_OUT,
   };
+};
+
+export const createStream = (formValues) => async (dispatch) => {
+  streams.post('/streams', formValues);
 };
---
+++ client/src/apis/streams.js
@@ -0,0 +1,5 @@
+impot axios from 'axios';
+
+export default axios.create({
+    baseURL: 'http://localhost:3001'
+})
