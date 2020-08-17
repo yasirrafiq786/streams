4df7306fd6466570950620357b072bf29548828e
Yasir Malik <yasirrafiq786@gmail.com>
2020-08-17 16:06:11 +1000

    App completed almost

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -10,7 +10,30 @@ class StreamShow extends React.Component {
     this.videoRef = React.createRef();
   }
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+    const {id} = this.props.match.params;
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    const {id} = this.props.match.params;
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
@@ -20,7 +43,7 @@ class StreamShow extends React.Component {
     const {title, description} = this.props.stream;
     return (
       <div>
-        <video ref={this.videoRef} />
+        <video ref={this.videoRef} style={{width: '100%'}} controls />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
