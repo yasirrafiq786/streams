f86e0905a249055f14acb0052f3b6e33ce4433d7
Yasir Malik <yasirrafiq786@gmail.com>
2020-08-17 14:22:45 +1000

    started making show component

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -7,19 +7,20 @@ import {
   CREATE_STREAM,
 } from '../actions/types';
 
-export default (state ={}, action) => {
-    switch (action.type) {
-        case FETCH_STREAM:
-            return { ...state, [action.payload.id]: action.payload }
-        case CREATE_STREAM:
-            return { ...state, [action.payload.item]: action.payload }
-        case EDIT_STREAM:
-            return { ...state, [action.payload.id]: action.payload }
-        case DELETE_STREAM:
-            return _.omit(state, action.payload);
-        case FETCH_STREAMS:
-            return {...state, ..._.mapKeys(action.payload, 'id')};
-        default:
-            return state
-    }
-}
+export default (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAMS:
+      return {...state, ..._.mapKeys(action.payload, 'id')};
+    case FETCH_STREAM:
+      return {...state, [action.payload.id]: action.payload};
+    case CREATE_STREAM:
+      return {...state, [action.payload.item]: action.payload};
+    case EDIT_STREAM:
+      return {...state, [action.payload.id]: action.payload};
+    case DELETE_STREAM:
+      return _.omit(state, action.payload);
+
+    default:
+      return state;
+  }
+};
